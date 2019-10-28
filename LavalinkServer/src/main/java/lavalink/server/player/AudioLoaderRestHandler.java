ed8875452820bdfc2a9b3da6a23d0573702cd649
Zenrac <zenrac@outlook.fr>
2019-10-28 14:43:13 +0100

    forgot to remove this

--- LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
+++ LavalinkServer/src/main/java/lavalink/server/player/AudioLoaderRestHandler.java
@@ -72,7 +72,7 @@ public class AudioLoaderRestHandler {
                 .put("uri", trackInfo.uri)
                 .put("isStream", trackInfo.isStream)
                 .put("isSeekable", audioTrack.isSeekable())
-                .put("position", audioTrack.getPosition());
+                .put("position", audioTrack.getPosition())
                 .put("artwork", trackInfo.getArtworkUrl());       
     }
 
